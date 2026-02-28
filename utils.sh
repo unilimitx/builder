3997c28cd384d9de3e6564a5e97e3ab363ffc4f8
peternmuller <pm563838@gmail.com>
2026-02-28 16:46:09 +0100

    builder: Update Firefox User-Agent
    Signed-off-by: peternmuller <pm563838@gmail.com>

--- utils.sh
+++ utils.sh
@@ -221,7 +221,7 @@ _req() {
 		mv -f "$dlp" "$op"
 	fi
 }
-req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:147.0) Gecko/20100101 Firefox/147.0"; }
+req() { _req "$1" "$2" -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:148.0) Gecko/20100101 Firefox/148.0"; }
 gh_req() { _req "$1" "$2" -H "$GH_HEADER"; }
 gh_dl() {
 	if [ ! -f "$1" ]; then
